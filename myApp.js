e0f46e8c9cc03203ec11af86754a942f39cf1e7e
Sérgio Brandão <srgb88@gmail.com>
2023-06-13 22:55:10 +0100

    myApp.js update

--- myApp.js
+++ myApp.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
 
-
+(getUserInput) =>
+  $.get(getUserInput('url') + '/_api/package.json').then(
+    (data) => {
+      const packJson = JSON.parse(data);
+      const helmet = packJson.dependencies.helmet;
+      assert(helmet === '3.21.3' || helmet === '^3.21.3');
+    },
+    (xhr) => {
+      throw new Error(xhr.responseText);
+    }
+  );
 
 
 
