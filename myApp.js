dd9c46ab41dab3037a637815d4ca15a379474529
Sérgio Brandão <srgb88@gmail.com>
2023-06-13 23:04:19 +0100

    myApp.js require helmet

--- myApp.js
+++ myApp.js
@@ -1,22 +1,7 @@
 const express = require('express');
 const app = express();
 
-(getUserInput) =>
-  $.get(getUserInput('url') + '/_api/package.json').then(
-    (data) => {
-      const packJson = JSON.parse(data);
-      const helmet = packJson.dependencies.helmet;
-      assert(helmet === '3.21.3' || helmet === '^3.21.3');
-    },
-    (xhr) => {
-      throw new Error(xhr.responseText);
-    }
-  );
-
-
-
-
-
+helmet
 
 
 
